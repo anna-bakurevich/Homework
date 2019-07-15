8768604af2212becb0438317a7db55e78aeb4fd4
Anna Bakurevich <annabaku0505@gmail.com>
2019-07-15 19:27:09 +0300

    Lesson5. Wrappers

--- src/lesson5/IntegerDouble.java
+++ src/lesson5/IntegerDouble.java
@@ -1,12 +1,23 @@
 package lesson5;
+/*
+Определить класс, который при инициализации принимает на вход 2 аргумента целого или вещественного типа.
+Задать перегруженные методы возвращающие результат арифметических операций над заданными полями
+и полями типа BigDecimal.
+ */
+
+import lombok.Getter;
 
 import java.math.BigDecimal;
+
+@Getter
 
 public class IntegerDouble {
     private int i1;
     private int i2;
     private double d1;
     private double d2;
+    private BigDecimal bd1;
+    private BigDecimal bd2;
 
     public IntegerDouble(int i1, int i2) {
         this.i1 = i1;
@@ -18,44 +29,32 @@ public class IntegerDouble {
         this.d2 = d2;
     }
 
-    public int getI1() {
-        return i1;
+    public IntegerDouble(BigDecimal bd1, BigDecimal bd2) {
+        this.bd1 = bd1;
+        this.bd2 = bd2;
     }
 
-    public int getI2() {
-        return i2;
-    }
-
-    public double getD1() {
-        return d1;
-    }
-
-    public double getD2() {
-        return d2;
-    }
-
-
-    public int sumInteger() {
+    public int sum(int i1, int i2) {
         return i1 + i2;
     }
 
-    public double sumDouble() {
+    public double sum(double d1, double d2) {
         return d1 + d2;
     }
 
-    public int multiplicationInteger() {
+    public BigDecimal sum(BigDecimal bd1, BigDecimal bd2) {
+        return bd1.add(bd2);
+    }
+
+    public int multiply(int i1, int i2) {
         return i1 * i2;
     }
 
-    public double multiplicationDouble() {
+    public double multiply(double d1, double d2) {
         return d1 * d2;
     }
 
-    public BigDecimal sumBigDecimal() {
-        return new BigDecimal(d1).add(new BigDecimal(d2));
-    }
-
-    public BigDecimal multiplicationBigDecimal(){
-        return new BigDecimal(d1).multiply(new BigDecimal(d2));
+    public BigDecimal multiply(BigDecimal bd1, BigDecimal bd2) {
+        return bd1.multiply(bd2);
     }
 }
--- src/lesson5/Lesson5Task3.java
+++ src/lesson5/Lesson5Task3.java
@@ -1,23 +1,20 @@
 package lesson5;
 
-/*
-Определить класс, который при инициализации принимает на вход 2 аргумента целого или вещественного типа.
-Задать перегруженные методы возвращающие результат арифметических операций над заданными полями
-и полями типа BigDecimal.
- */
+import java.math.BigDecimal;
 
 public class Lesson5Task3 {
     public static void main(String[] args) {
         IntegerDouble id1 = new IntegerDouble(5,10);
         IntegerDouble id2 = new IntegerDouble(5.555, 10.333);
+        IntegerDouble id3 = new IntegerDouble(new BigDecimal(5.555), new BigDecimal(10.333));
         System.out.println("int");
-        System.out.println(id1.getI1() + " + " + id1.getI2() + " = " + id1.sumInteger());
-        System.out.println(id1.getI1() + " * " + id1.getI2() + " = " + id1.multiplicationInteger());
+        System.out.println(id1.getI1() + " + " + id1.getI2() + " = " + id1.sum(id1.getI1(),id2.getI2()));
+        System.out.println(id1.getI1() + " * " + id1.getI2() + " = " + id1.multiply(id1.getI1(), id2.getI2()));
         System.out.println("double");
-        System.out.println(id2.getD1() + " + " + id2.getD2() + " = " + id2.sumDouble());
-        System.out.println(id2.getD1() + " * " + id2.getD2() + " = " + id2.multiplicationDouble());
+        System.out.println(id2.getD1() + " + " + id2.getD2() + " = " + id2.sum(id2.getD1(),id2.getD2()));
+        System.out.println(id2.getD1() + " * " + id2.getD2() + " = " + id2.multiply(id2.getD1(),id2.getD2()));
         System.out.println("BigDecimal");
-        System.out.println(id2.getD1() + " + " + id2.getD2() + " = " + id2.sumBigDecimal());
-        System.out.println(id2.getD1() + " * " + id2.getD2() + " = " + id2.multiplicationBigDecimal());
+        System.out.println(id3.getBd1() + " + " + id3.getBd2() + " = " + id3.sum(id3.getBd1(), id3.getBd2()));
+        System.out.println(id3.getBd1() + " * " + id3.getBd2() + " = " + id3.multiply(id3.getBd1(), id3.getBd2()));
     }
 }
