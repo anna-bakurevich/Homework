54061dd83b88a7da41fbf74ea400717e921aaec0
Anna Bakurevich <annabaku0505@gmail.com>
2019-08-09 20:21:30 +0300

    Lesson20. ThreadExecutors

--- src/lesson20/Lesson20Task2.java
+++ src/lesson20/Lesson20Task2.java
@@ -14,12 +14,18 @@ public class Lesson20Task2 {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         //создаем масссив случайных целых чисел (от 1 до 300) случайной длины (1 млн элементов)
         int[] array = new int[randomInteger(1_000_000)];
-        System.out.println(array.length);
+        System.out.printf("Длина массива: %,d элементов\n", array.length);
         for (int i = 0; i < array.length; i++) {
             array[i] = randomInteger(300);
         }
+        long startTime = System.nanoTime();
         maxElementOneThread(array);
+        long finishTime = System.nanoTime();
+        System.out.printf("Способ вычисления в одном потоке занял %,d нс\n", (finishTime - startTime));
+        startTime = System.nanoTime();
         maxElementTenThreads(array);
+        finishTime = System.nanoTime();
+        System.out.printf("Способ вычисления при помощи 10 потоков занял %,d нс\n", (finishTime - startTime));
     }
 
     public static int maxElementOneThread(int[] array) {
@@ -34,14 +40,15 @@ public class Lesson20Task2 {
     public static int maxElementTenThreads(int[] array) throws ExecutionException, InterruptedException {
         ExecutorService service = Executors.newFixedThreadPool(10);
         List<Future<Integer>> tasks = new ArrayList<>();
-        for (int i = 0; i < array.length; i += array.length/10) {
-            int start = i;
-            System.out.println(i);
+        int blockSize = (array.length + 9) / 10;
+        for (int i = 0; i < 10; i++) {
+            int start = blockSize * i;
+            int finish = Math.min(blockSize * (i + 1), array.length);
             tasks.add(service.submit(new Callable<Integer>() {
                 @Override
                 public Integer call() throws Exception {
                     int max = array[0];
-                    for (int j = start; j < start + array.length/10; j++) {
+                    for (int j = start; j < finish; j++) {
                         max = Math.max(max, array[j]);
                     }
                     return max;
